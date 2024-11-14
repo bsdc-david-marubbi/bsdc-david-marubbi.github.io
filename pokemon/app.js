a9988a12eba7af6ab7dbc2f87c627e68aca80dd0
David Marubbi <david.marubbi@bsdc.ac.uk>
2024-11-14 12:30:59 +0000

    pokemon

--- pokemon/app.js
+++ pokemon/app.js
@@ -1,10 +1,16 @@
 console.log("pokemon app");
 
 const container = document.querySelector("#container");
-const pokemonurl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
+const pokemonurl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`;
 
 for (i = 1; i <= 151; i++) {
+  const pokemon = document.createElement("div");
+  const label = document.createElement("span");
+  label.innerText = `${i}`;
   const newImg = document.createElement("img");
   newImg.src = `${pokemonurl}${i}.png`;
-  container.appendChild(newImg);
+
+  pokemon.appendChild(newImg);
+  pokemon.appendChild(label);
+  container.appendChild(pokemon);
 }
