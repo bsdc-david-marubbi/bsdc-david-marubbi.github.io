9f31d33fc4bff6277e0e3d25cc329c79d411f955
David Marubbi <david.marubbi@bsdc.ac.uk>
2024-11-26 11:16:33 +0000

    James disabled

--- forms/app.js
+++ forms/app.js
@@ -3,28 +3,18 @@ console.log("Form App running");
 const myForm = document.getElementById("myForm");
 console.log(myForm);
 
-myForm.addEventListener("submit", function (e) {
-    e.preventDefault();
- console.log('button pressed')
- 
- const newRow = document.createElement("tr");
- newRow.innerHTML = `<td>${inputName.value}</td><td>${inputEmail.value}</td>`;
- outputTable.appendChild(newRow);
+myForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("button pressed", event);
 
- inputName.value='';
- inputEmail.value='';
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `<td>${inputName.value}</td><td>${inputEmail.value}</td>`;
+  outputTable.appendChild(newRow);
 
-} )
+  inputName.value = "";
+  inputEmail.value = "";
+});
 
 const outputTable = document.getElementById("outputTable");
 const inputName = document.getElementById("name");
 const inputEmail = document.getElementById("email");
-
-
-// myForm.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   console.log("the form was submitted");
-//   console.log(e);
-
-
-// });
